dee8c5f25f538dae0e1b5b01b3347d92cfcc1d46
Zweer <niccolo@olivieriachille.com>
2017-01-13 17:13:59 +0100

    Re-written the "set" model to fit the Scraper

--- __tests__/models/set.js
+++ __tests__/models/set.js
@@ -8,34 +8,71 @@ describe('Set', () => {
     });
 
     it('should have all the attributes', () => {
-      expect(Set.rawAttributes.Nname).toBeDefined();
-      expect(Set.rawAttributes.Ncode).toBeDefined();
-      expect(Set.rawAttributes.Ncode_magiccards).toBeDefined();
-      expect(Set.rawAttributes.Ndate).toBeDefined();
-      expect(Set.rawAttributes.Nis_promo).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_nM).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_nR).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_nU).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_nC).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_nE).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_pM).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_pR).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_typeExtra1).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_typeExtra2).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_listExtra1).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_listExtra2).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_has_foil).toBeDefined();
-      expect(Set.rawAttributes.Nboosterpack_pF).toBeDefined();
+      expect(Set.rawAttributes.code).toBeDefined();
+      expect(Set.rawAttributes.code.allowNull).toBe(false);
+      expect(Set.rawAttributes.code.primaryKey).toBe(true);
+
+      expect(Set.rawAttributes.language).toBeDefined();
+      expect(Set.rawAttributes.language.allowNull).toBe(false);
+      expect(Set.rawAttributes.language.primaryKey).toBe(true);
+
+      expect(Set.rawAttributes.name).toBeDefined();
+      expect(Set.rawAttributes.name.allowNull).toBe(false);
+
+      expect(Set.rawAttributes.createdAt).toBeDefined();
+      // eslint-disable-next-line no-underscore-dangle
+      expect(Set._timestampAttributes.createdAt).toBe('createdAt');
+
+      expect(Set.rawAttributes.updatedAt).toBeDefined();
+      // eslint-disable-next-line no-underscore-dangle
+      expect(Set._timestampAttributes.updatedAt).toBe('updatedAt');
     });
   });
 
   describe('Instance', () => {
-    beforeEach(() => sequelize.sync());
+    const code = 'aaa';
+    const language = 'en';
+    const name = 'set a';
 
-    it('should have a row', () => Set.findAll()
+    beforeEach(() => sequelize.sync({ force: true })
+      .then(() => Promise.all([
+        Set.create({ code, language, name }),
+      ])));
+
+    it('should have 1 row', () => Set.findAll()
       .then(sets => expect(sets).toHaveLength(1)));
 
-    it('should have the "ARB" row', () => Set.findOne({ where: { Ncode: { $like: 'ARB' } } })
-      .then(set => expect(set).not.toBeNull()));
+    it('should have the "aaa" row', () => Set.findOne({ where: { code } })
+      .then((set) => {
+        expect(set).toBeDefined();
+        expect(set).toBeInstanceOf(Object);
+        expect(set.name).toBe(name);
+      }));
+
+    describe('Validation', () => {
+      it('should not create a row without the "code" field', () => Set.create({ language, name })
+        .then(() => { throw new Error('This test should throw an exception'); })
+        .catch((err) => {
+          expect(err).toBeDefined();
+          expect(err.name).toBe('SequelizeValidationError');
+          expect(err.errors[0].path).toBe('code');
+        }));
+
+      it('should not create a row without the "language" field', () => Set.create({ code, name })
+        .then(() => { throw new Error('This test should throw an exception'); })
+        .catch((err) => {
+          expect(err).toBeDefined();
+          expect(err.name).toBe('SequelizeValidationError');
+          expect(err.errors[0].path).toBe('language');
+        }));
+
+      it('should not create a row without the "name" field', () => Set.create({ code, language })
+        .then(() => { throw new Error('This test should throw an exception'); })
+        .catch((err) => {
+          expect(err).toBeDefined();
+          expect(err.name).toBe('SequelizeValidationError');
+          expect(err.errors[0].path).toBe('name');
+        }));
+    });
   });
 });
--- models/set.js
+++ models/set.js
@@ -1,29 +1,22 @@
 export default function (sequelize, DataTypes) {
   return sequelize.define('set', {
-    Nname: DataTypes.TEXT,
-    Ncode: {
-      type: DataTypes.TEXT,
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false,
       primaryKey: true,
     },
-    Ncode_magiccards: DataTypes.TEXT,
-    Ndate: DataTypes.TEXT,
-    Nis_promo: DataTypes.TEXT,
-    Nboosterpack_nM: DataTypes.TEXT,
-    Nboosterpack_nR: DataTypes.TEXT,
-    Nboosterpack_nU: DataTypes.TEXT,
-    Nboosterpack_nC: DataTypes.TEXT,
-    Nboosterpack_nE: DataTypes.TEXT,
-    Nboosterpack_pM: DataTypes.TEXT,
-    Nboosterpack_pR: DataTypes.TEXT,
-    Nboosterpack_typeExtra1: DataTypes.TEXT,
-    Nboosterpack_typeExtra2: DataTypes.TEXT,
-    Nboosterpack_listExtra1: DataTypes.TEXT,
-    Nboosterpack_listExtra2: DataTypes.TEXT,
-    Nboosterpack_has_foil: DataTypes.TEXT,
-    Nboosterpack_pF: DataTypes.TEXT,
+
+    language: {
+      type: DataTypes.STRING(2),
+      allowNull: false,
+      primaryKey: true,
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   }, {
-    tableName: 'MP_sets',
-    timestamps: false,
     classMethods: {
       associate(/* models */) {},
     },
