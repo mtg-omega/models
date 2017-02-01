4da21522014470b7aade4379ff91c8eee0c18805
Zweer <niccolo@olivieriachille.com>
2017-02-01 14:03:00 +0100

    Start from the beginning

--- sql/card.js
+++ sql/card.js
@@ -1,65 +1,22 @@
-export default function (sequelize, DataTypes) {
-  return sequelize.define('card', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4(),
-    },
+// code
+// language
+// index
+// name
+// typeStr
+// type
+// superType
+// subType
+// power
+// toughness
+// loyalty
+// mana
+// rarity
+// artist
 
-    index: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-    },
-
-    name: {
-      type: DataTypes.STRING,
-    },
-
-    typeStr: {
-      type: DataTypes.STRING,
-    },
-
-    type: {
-      type: DataTypes.STRING,
-    },
-
-    superType: {
-      type: DataTypes.STRING,
-    },
-
-    subType: {
-      type: DataTypes.STRING,
-    },
-
-    power: {
-      type: DataTypes.INTEGER,
-    },
-
-    toughness: {
-      type: DataTypes.INTEGER,
-    },
-
-    loyalty: {
-      type: DataTypes.INTEGER,
-    },
-
-    mana: {
-      type: DataTypes.STRING,
-    },
-
-    rarity: {
-      type: DataTypes.STRING,
-    },
-
-    artist: {
-      type: DataTypes.STRING,
-    },
-  }, {
+export default function (sequelize/* , DataTypes */) {
+  return sequelize.define('card', {}, {
     classMethods: {
-      associate(models) {
-        models.card.belongsTo(models.set);
-      },
+      associate(/* models */) {},
     },
   });
 }
--- sql/set.js
+++ sql/set.js
@@ -1,30 +1,11 @@
-export default function (sequelize, DataTypes) {
-  return sequelize.define('set', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4(),
-    },
+// code
+// language
+// name
 
-    code: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-    language: {
-      type: DataTypes.STRING(2),
-      allowNull: false,
-    },
-
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  }, {
+export default function (sequelize/* , DataTypes */) {
+  return sequelize.define('set', {}, {
     classMethods: {
-      associate(models) {
-        models.set.hasMany(models.card);
-      },
+      associate(/* models */) {},
     },
   });
 }
